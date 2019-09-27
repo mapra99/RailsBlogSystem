68770eccefbc4bf19940baac5857cd2905ab4f9f
Miguel Prada <mapra99@gmail.com>
2019-09-27 16:13:17 -0500

    deleted destroy action

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,18 +1,9 @@
 class TagsController < ApplicationController
     def show
         @tag = Tag.find(params[:id])
-    end  
+    end
 
     def index
         @tags = Tag.all
-    end
-
-    def destroy
-        @tag = Tag.find(params[:id])
-        @tag.destroy
-
-        flash.notice = "Your tag '#{@tag.name}' has been removed."
-
-        redirect_to tags_path
     end
 end
